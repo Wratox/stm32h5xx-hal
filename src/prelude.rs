8352a7be1578063bf0111118a7222de2a73e5689
Henrik Snöman <henrik.snoman@dectron.se>
2025-01-23 11:55:29 +0100

    Added RNG
    Added example
    
    Fixed some requested changes
    
    Follow example configurations in RM0481 and RM0492
    
    fmt
    
    Updated to latest and fixed some desired changes
    
    Formatting
    
    Added CryptRng and modes
    
    Fixed missing field
    
    Fixed requested changes
    
    Changed iterator to work with result instead
    
    Changed clock error handling
    
    Removed clock error type
    
    Added logging on clock error
    
    Added logging for seed error
    
    Fix for clippy
    
    Fixed missed error
    
    Fixed formatting

--- src/prelude.rs
+++ src/prelude.rs
@@ -8,5 +8,6 @@ pub use crate::pwr::PwrExt as _stm32h5xx_hal_pwr_PwrExt;
 pub use crate::rcc::RccExt as _stm32h5xx_hal_rcc_RccExt;
 pub use crate::spi::SpiExt as _stm32h5xx_hal_spi_SpiExt;
 
+pub use crate::rng::{RngCore as _, RngExt as _};
 pub use crate::time::U32Ext as _;
 pub use fugit::{ExtU32 as _, RateExtU32 as _};
